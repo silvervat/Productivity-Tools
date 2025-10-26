607c16d56e083af96f324197e06d269b33c1ec30
silvervat <silver.vatsel@gmail.com>
2025-10-26 18:42:14 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { defineCustomElements } from '@trimble-oss/modus-web-components/loader';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // <-- ilma .tsx laiendita
 
-defineCustomElements();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
